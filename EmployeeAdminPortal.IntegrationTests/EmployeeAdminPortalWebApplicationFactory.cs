a60b61f646e8f7185fd6b11b141d59c26700b685
learnerSudam <sudamswain555@gmail.com>
2025-02-07 07:07:02 +0530

    adding all project files

---
+++ EmployeeAdminPortal.IntegrationTests/EmployeeAdminPortalWebApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿
+
+using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace EmployeeAdminPortal.IntegrationTests;
+
+internal class EmployeeAdminPortalWebApplicationFactory : WebApplicationFactory<Program>, IDisposable, IAsyncDisposable
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+            var connectionString = GetConnectionString();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+                    
+                });
+            });
+
+            // Ensure the database is created fresh and apply migrations
+            var sp = services.BuildServiceProvider();
+            using (var scope = sp.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.EnsureDeleted(); // Delete the existing database
+                db.Database.Migrate(); // Apply migrations to create the database schema
+            }
+
+        });
+    }
+
+    private static string? GetConnectionString()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<EmployeeAdminPortalWebApplicationFactory>()
+            .Build();
+        var connectionString = configuration.GetConnectionString("TestConnection");
+        return connectionString;
+    }
+
+}
---
+++ EmployeeAdminPortal.IntegrationTests/EmployeesControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+
+namespace EmployeeAdminPortal.IntegrationTests
+{
+    public class EmployeesControllerTests : IClassFixture<EmployeeAdminPortalWebApplicationFactory>
+    {
+
+
+        public EmployeesControllerTests()
+        {
+            
+        }
+        [Fact]
+        public async Task GetEmployees_Request_responds_with_OK()
+        {
+
+            // Arrange
+            var application = new EmployeeAdminPortalWebApplicationFactory();
+            var _client = application.CreateClient();
+            // Act
+            var response = await _client.GetAsync("api/employees");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Asserts that the status code is 200 (OK)
+            var getAllEmployeesResponse = await response.Content.ReadAsStringAsync();
+            getAllEmployeesResponse?.Should().NotBeNullOrEmpty();
+        }
+    }
+}
+
